33d96a4996f05f49608d7d9d3803be7c5f044491
Sergio Rojas-Aguilar <128770839+Sergrojas29@users.noreply.github.com>
2026-03-15 12:08:47 -0400

    db test endpoint

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+import psycopg2
+
+
+
+
+
 app = Flask(__name__)
+
+
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("your_db_url_here") 
+    conn.close()
+    return  "Database Connection Successful"
+    
